bce29ad4d4a889970d61aee987c1d727bebc2338
shuyin <xu.liu@pku.edu.cn>
2021-02-13 23:16:09 +0800

    1

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -38,7 +38,7 @@ let clickChangeFruit = true;
 let adLink = false;
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成###';
+document.getElementsByTagName("title")[0].innerText = '甜筒后援会';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
